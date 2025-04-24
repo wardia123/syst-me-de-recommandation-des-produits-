d9c7a49c4531347c7f4f3936893409972fb17220
Wãr Dîa✨ <121893169+wardia123@users.noreply.github.com>
2025-04-24 18:57:50 +0100

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,20 @@
+package project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ecommerce_db";
+    private static final String USER = "root";
+    private static final String PASS = ""; // 🔒 à modifier selon ton système
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
